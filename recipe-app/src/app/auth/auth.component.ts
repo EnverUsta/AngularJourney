0e26f9f9b40649b0eef985e9a84a8c741f41de15
EnverUsta <enveruusta@gmail.com>
2022-06-26 01:10:53 +0300

    ngrx store continue

--- recipe-app/src/app/auth/auth.component.ts
+++ recipe-app/src/app/auth/auth.component.ts
@@ -6,10 +6,13 @@ import {
 } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 import { AlertComponent } from '../shared/alert/alert.component';
 import { PlaceHolderDirective } from '../shared/placeholder/placeholder.directive';
 import { AuthService } from './auth.service';
+import * as fromApp from '../store/app.reducer';
+import * as authActions from './store/auth.actions';
 
 @Component({
   selector: 'app-auth',
@@ -26,12 +29,12 @@ export class AuthComponent implements OnDestroy {
   constructor(
     private authService: AuthService,
     private router: Router,
-    private componentFactoryResolver: ComponentFactoryResolver
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private store: Store<fromApp.AppState>
   ) {}
 
   ngOnDestroy(): void {
-    if (this.closeSub)
-      this.closeSub.unsubscribe();
+    if (this.closeSub) this.closeSub.unsubscribe();
   }
 
   onSwitchMode() {
@@ -58,7 +61,10 @@ export class AuthComponent implements OnDestroy {
 
     this.isLoading = true;
     if (this.isLoginMode) {
-      this.authService.login(email, password).subscribe(authObserver);
+      // this.authService.login(email, password).subscribe(authObserver);
+      this.store.dispatch(
+        new authActions.LoginStart({ email: email, password: password })
+      );
     } else {
       this.authService.signup(email, password).subscribe(authObserver);
       authForm.reset();
